6f1f61b9f5a7faaab7326665a0ddf8fc09d26919
Xenonar <xenon_ark_88@hotmail.com>
2021-07-10 01:52:02 +0200

    Update index.js
    Remove some link

--- nextjs-ex/pages/index.js
+++ nextjs-ex/pages/index.js
@@ -30,7 +30,6 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
